ec99ad7ddb6677ef3c7fa0549e80eef5236d9d5a
Omoruyi <osakueomoruyi@gmail.com>
2023-02-22 11:36:47 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,17 @@
  *
  * Return: 0 when done
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf(\n);
 	printf("size of int: %lu  byte(s)", sizeof(int));
+	printf(\n);
 	printf("size of a long int: %lu byte(s)", sizeof(long int));
+	printf(\n);
 	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf(\n);
 	printf("size of a float: %lu byte(s)", sizeof(float));
+	printf(\n);
 	return (0);
 }
