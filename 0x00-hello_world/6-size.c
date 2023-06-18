0ad54a48de8fc53b1bc6ae7d0d6bd67fc6bd2032
Omoruyi <osakueomoruyi@gmail.com>
2023-06-18 08:48:03 -0700

    C progamming is awesome

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0 (success)
  */
-int main() {
-  printf("Size of char: %zu\n", sizeof(char));
-  printf("Size of short int: %zu\n", sizeof(short int));
-  printf("Size of int: %zu\n", sizeof(int));
-  printf("Size of long int: %zu\n", sizeof(long int));
-  printf("Size of float: %zu\n", sizeof(float));
-  printf("Size of double: %zu\n", sizeof(double));
-  printf("Size of long double: %zu\n", sizeof(long double));
-  return 0;
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of short int: %zu byte(s)\n", sizeof(short int));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of double: %zu byte(s)\n", sizeof(double));
+	printf("Size of long double: %zu byte(s)\n", sizeof(long double));
+	return (0);
 }
