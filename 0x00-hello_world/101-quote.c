8df0c147a8f944e690621e2df814755e016335cc
Omoruyi <osakueomoruyi@gmail.com>
2023-06-18 09:21:22 -0700

    C progamming is awesome

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - outputs a string
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fprintf(stderr, "%s", s);
+	return (1);
+}
